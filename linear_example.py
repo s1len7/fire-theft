91ab996c12a23000682314432c8baad72977a128
KO, HYEONG-IL <hyeongil.ko@gmail.com>
2017-09-05 17:49:56 +0900

    first commit

---
+++ linear_example.py
@@ -0,0 +1,42 @@
+"""
+    simple linear regression
+
+"""
+
+
+import tensorflow as tf
+from fire_theft import *
+
+#
+# Create model 
+#
+
+# placeholder
+
+# Variable
+
+# Linear function
+
+# loss function
+
+# Optimizer
+
+#
+#  Session run
+#
+with tf.Session() as sess:
+
+    # tensorflow variable init
+    
+    for i in range(100):
+
+        # 
+        
+        for x, y in fire_theft_data:
+            print('training..')
+            #  sess.run(...) for training
+
+
+    # print out trained variables
+    print('trained value: w=?, b=?')
+
